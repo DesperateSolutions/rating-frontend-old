169c29509da66f17c36085732f14ce732ac02a30
quist <joakiml@gmail.com>
2016-01-21 00:48:14 +0100

    Specify league name in config.

--- app/js/config/appConfig.js
+++ app/js/config/appConfig.js
@@ -1,3 +1,7 @@
+
+var leagueName = "squash";
+var apiUrl = "http://api.chess-rating.vegarm.svc.tutum.io:3000/" + leagueName;
+
 module.exports = {
-    apiUrl : "http://api.chess-rating.vegarm.svc.tutum.io:3000"
+    apiUrl : apiUrl
 };
