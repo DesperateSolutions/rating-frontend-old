3e3ccade9556e9fd9f92b96c7bb48fcb9500e26c
Henning Lund-Hanssen <hennilu@student.matnat.uio.no>
2016-02-04 00:56:41 +0100

    timestamp-parsing: needs testing - should work?

--- app/js/components/Game.jsx
+++ app/js/components/Game.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import GameActions from '../actions/GameActions';
+import moment from 'moment';
 
 export default class Game extends React.Component {
 
     constructor() {
         super();
+        moment.format();
     }
 
     handleDelete() {
         GameActions.deleteGame(this.props.game._id);
+    }
+
+    timestampToDate(timestamp) {
+        gameDate = moment(timestamp);
+
+        if (!gameDate.isValid()) {
+            return 'Invalid date';
+        }
+
+        return gameDate.format('D MMM');
     }
 
     render() {
@@ -17,6 +29,7 @@ export default class Game extends React.Component {
                 <th>{this.props.game.white}</th>
                 <th>{this.props.game.black}</th>
                 <th>{this.props.game.result}</th>
+                <th>{timestampToDate(this.props.game.timestamp)}</th>
                 <th><a className="secondary-content action-link" onClick={this.handleDelete.bind(this)}><i className="material-icons">delete</i></a></th>
             </tr>
         );
