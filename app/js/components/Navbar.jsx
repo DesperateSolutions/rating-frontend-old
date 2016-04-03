27bc6d98480a5d5ac0ec2f79f6cd995f70f463a7
quist <joakiml@gmail.com>
2016-04-04 00:26:30 +0200

    Improve mobile friendliness

--- app/js/components/Navbar.jsx
+++ app/js/components/Navbar.jsx
@@ -12,22 +12,23 @@ export default class Navbar extends React.Component {
         return (
             <nav className="green">
                 <div className="nav-wrapper container">
-                    <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
                     <ul className="left hide-on-med-and-down">
                         <li><Link to="/game">Add game</Link></li>
                         <li><Link to="/ranking">Ranking</Link></li>
                         <li><Link to="/games">Games</Link></li>
                         <li><Link to="/ranking">Leagues</Link></li>
                     </ul>
-                    <ul className="side-nav" id="mobile-demo">
+                    <ul className="side-nav" id="slide-out">
                         <li><Link to="/game">Add game</Link></li>
                         <li><Link to="/ranking">Ranking</Link></li>
                         <li><Link to="/games">Games</Link></li>
                         <li><Link to="/ranking">Leagues</Link></li>
                     </ul>
+                    <a href="#" data-activates="slide-out" className="button-collapse"><i className="mdi-navigation-menu"></i></a>
                     <a href="/" className="brand-logo right">{Config.leagueName}</a>
                 </div>
             </nav>
         );
     }
 }
+
