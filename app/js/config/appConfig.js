973d8ff8a7970b249307dc431bdb1b208c28eb92
Joakim Lindquister <joakiml@gmail.com>
2016-04-04 20:59:28 +0200

    Update appConfig.js

--- app/js/config/appConfig.js
+++ app/js/config/appConfig.js
@@ -1,6 +1,6 @@
 
 var leagueName = "squash";
-var baseUrl = "http://glicko-api.desperate.solutions:3001/";
+var baseUrl = "http://glicko-api.desperate.solutions:3000/";
 var apiUrl = baseUrl + leagueName;
 
 
